239e7960779383cd758646b15a7795038dfb1417
Dan James <dan@danarchy.me>
2019-06-23 10:22:05 -0700

    duplicate script without timeouts

---
+++ openstack_test_no_timeout.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'timeout'
+require 'json'
+require 'fog/openstack'
+
+connection = JSON.parse(File.read('./connection.json'), symbolize_names: true)
+
+puts "Conecting to OpenStack: #{connection[:openstack_auth_url]}"
+os = Fog::OpenStack::Compute.new(connection)
+
+if os
+  puts 'Successfully connected to OpenStack!'
+
+  puts "Checking instance count..."
+  instances = os.servers.count
+  end
+
+  if instances
+    puts "Instance Count: #{instances}"
+  else
+    puts "Failed to get instance count"
+  end
+
+  puts "Checking security group count..."
+  secgroups = os.security_groups.count
+  end
+
+  if secgroups
+    puts "Security Group Count: #{secgroups}"
+  else
+    puts "Failed to get security group count"
+  end
+else
+  puts "Failed to connect to OpenStack"
+end
+  
+puts '---------------------------'
